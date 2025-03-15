4983a90833299fc6900d9b41d7e05640dc64dc3d
Samuel de Moraes Delgado <samueldemoraesdelgado2005@gmail.com>
2025-03-15 09:36:11 -0300

    Exercícios

---
+++ aula21_composicao/run.py
@@ -0,0 +1,20 @@
+class Select:
+    def by_id(self) -> any:
+        print("Selecionando um elemento no BD")
+    
+    
+class Insert:
+    def inser_value(self) -> None:
+        print("Inserindo um valor no BD")
+
+class Repositorio:
+    def __init__(self) -> None:
+        self.__select = Select() #Composição
+        self.__insert = Insert()
+        
+    def select_by_id(self, id : int) -> any:
+        self.__select.by_id()
+        
+        
+repo = Repositorio()
+repo.select_by_id(45)
